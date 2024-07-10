39067a167ea5e87b4044f879f36513c3511e7876
TanerSaydam <tanersaydam@gmail.com>
2024-07-10 20:22:01 +0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: "login",
+        loadComponent: ()=> import("./components/login/login.component")
+    },
+    {
+        path: "",
+        loadComponent: ()=> import("./components/layouts/layouts.component"),
+        children: [
+            {
+                path: "",
+                loadComponent: ()=> import("./components/home/home.component")
+            }
+        ]
+    },
+    {
+        path: "**",
+        loadComponent: ()=> import("./components/not-found/not-found.component")
+    }
+];
